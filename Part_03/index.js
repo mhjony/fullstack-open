e7016b2c7bace142b451f91e9de79e88bb63edda
mhjony <mahmudul.jhony@gmail.com>
2021-03-30 23:59:24 +0300

    part 3 before exercise

--- Part_03/index.js
+++ Part_03/index.js
@@ -1,4 +1,7 @@
-const http = require('http')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
 
 
 let notes = [
@@ -21,10 +24,53 @@ let notes = [
       important: true
     }
   ]
-  const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(notes))
-  })
+ 
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello world!</h1>');
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes);
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note)
+  {
+    console.log(note)
+    response.json(note)
+  }
+  else
+    response.status(404).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+  if (!body.content){
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(notes)
+})
 
 const PORT = 3001
 app.listen(PORT)
