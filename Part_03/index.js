b9cce11d80842cb2a7ed7cab6c30b2ee20bfc38e
Mahmudul <mahmudul@dev.datacrunch.io>
2021-07-07 21:00:46 +0300

    part03 exercise 3.1 - 3.8

--- Part_03/index.js
+++ Part_03/index.js
@@ -1,77 +1,102 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
 
-app.use(express.json())
+app.use(express.json());
 
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint' });
+};
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2019-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2019-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2019-05-30T19:20:14.298Z",
-      important: true
-    }
-  ]
- 
+let phonebooks = [
+	{
+		id: 1,
+		name: 'Arto Hellas',
+		number: '040-981562',
+	},
+	{
+		id: 2,
+		name: 'Ada Lovelace',
+		number: '050-081563',
+	},
+	{
+		id: 3,
+		name: 'Dan Abramov',
+		number: '060-181564',
+	},
+	{
+		id: 4,
+		name: 'Marry Poppendick',
+		number: '070-281565',
+	},
+];
+
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
+	const id = Math.floor(Math.random() * 1000 + 1);
+	return id;
+};
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello world!</h1>');
-})
+//exercise 3.7 && 3.8
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes);
-})
+// exercise 3.1
+app.get('/api/persons', (request, response) => {
+	response.json(phonebooks);
+});
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note)
-  {
-    console.log(note)
-    response.json(note)
-  }
-  else
-    response.status(404).end()
-})
+// exercise 3.2
+app.get('/info', (request, response) => {
+	const len = phonebooks.length;
+	response.send(`<p>Phonebook has info for ${len} people</p> <br />${Date()}`);
+});
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-  if (!body.content){
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
+// exercise 3.3
+app.get('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id);
+	const phonebook = phonebooks.find((pb) => pb.id === id);
+	if (phonebook) {
+		response.json(phonebook);
+	} else response.status(404).end();
+});
 
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
+// exercise 3.4
+app.delete('/api/persons/delete', (request, response) => {
+	const body = request.body;
+	const num = body.number;
+	const index = phonebooks.findIndex((pb) => pb.number === num);
+	if (index) {
+		phonebooks.splice(index, 1);
+		response.json(phonebooks);
+	} else {
+		return response.status(400).json({
+			error: 'Phone number not found',
+		});
+	}
+});
 
-  notes = notes.concat(note)
+// exercise 3.5 && 3.6
+app.post('/api/persons', (request, response) => {
+	const body = request.body;
+	if (!body.name || !body.number) {
+		return response.status(400).json({ error: 'request should contain name and number' });
+	}
+	const contactName = phonebooks.find((pb) => pb.name === body.name);
+	if (contactName) {
+		return response.status(401).json({ error: 'Name should be unique' });
+	}
+	const phonebook = {
+		id: generateId(),
+		name: body.name,
+		number: body.number,
+	};
 
-  response.json(notes)
-})
+	phonebooks = phonebooks.concat(phonebook);
+	response.json(phonebooks);
+});
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.use(unknownEndpoint);
+
+const PORT = 3003;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
